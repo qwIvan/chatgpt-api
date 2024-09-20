e84973536d900e06de8caaec861633ed5598a61b
Ivan <7613160+qwIvan@users.noreply.github.com>
2024-09-21 05:29:13 +0800

    uuid支持到v8

--- build/index.js
+++ build/index.js
@@ -421,7 +421,7 @@ import pTimeout2 from "p-timeout";
 import { v4 as uuidv42 } from "uuid";
 
 // src/utils.ts
-var uuidv4Re = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+var uuidv4Re = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 function isValidUUIDv4(str) {
   return str && uuidv4Re.test(str);
 }
